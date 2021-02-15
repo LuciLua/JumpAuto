f77ced2bf31390fffb577b9d0596aa30f2994872
luci <lucia81@protonmail.com>
2021-02-15 19:17:32 -0300

    Jump! Auto: Transformice

--- Transformice.py
+++ Transformice.py
@@ -4,8 +4,6 @@
 import pyautogui, time, msvcrt, keyboard
 time.sleep(1)
 
-print(pyautogui.position())
-
 while True:
     if keyboard.is_pressed("<"):
 
@@ -43,22 +41,3 @@ while True:
 else:
     print('interrompe')
     exit()
-
-'''
-for word in f:
-
-    pyautogui.keyDown('right')
-    time.sleep(0.15)
-    pyautogui.keyUp('right')
-
-    pyautogui.keyDown('up')
-    time.sleep(0.01)
-    pyautogui.keyUp('up')
-
-    pyautogui.keyDown('left')
-    time.sleep(0.02)
-    pyautogui.keyUp('left')
-
-exit()
-
-'''
